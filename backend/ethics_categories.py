7de57bd843f9105a71e24285d62e49fd53c0f7d0
desa-thomas <desa2thomas@gmail.com>
2025-06-05 20:50:02 -0400

    fixed quotation marks for queries

--- backend/ethics_categories.py
+++ backend/ethics_categories.py
@@ -5,13 +5,13 @@ Dict of categories and corresponding keywords for categorizing news headlines in
 """
 ETHICS_CATEGORIES = {
     "labor": [
-        "strike", "union", "layoff", "wage", "gig", "picket", "overtime", "worker", "pay gap"
+        "strike", "union", "layoff", "wage", "gig", "picket", "overtime", "worker"
     ],
     "environment": [
-        "pollution", "carbon", "climate", "oil spill", "recycle", "greenhouse", "e-waste", "deforestation"
+        "pollution", "carbon", "climate", '"oil spill"', "recycle", "greenhouse", "deforestation"
     ],
     "privacy": [
-        "data breach", "GDPR", "hack", "phishing", "spyware", "PII", "encryption", "tracking", "surveillance"
+        '"data breach"', "GDPR", "hack", "phishing", "spyware", "PII", "encryption", "tracking", "surveillance"
     ],
     "governance": [
         "fraud", "bribery", "lawsuit", "corruption", "whistleblower", "SEC", "scandal", "audit"
@@ -20,13 +20,12 @@ ETHICS_CATEGORIES = {
         "discrimination", "gender", "race", "DEI", "harassment", "bias", "equality", "inclusion"
     ],
     "human rights": [
-        "child labor", "forced labor", "genocide", "torture", "slavery", "detention", "repression", "censorship"
+        '"child labor"', '"forced labor"', "genocide", "torture", "slavery", "detention", "repression", "censorship"
     ],
     "consumer safety": [
-        "recall", "FDA", "toxicity", "defect", "contamination", "side effect", "unsafe product"
+        "recall", "FDA", "toxicity", "defect", "contamination", '"side effect"', '"unsafe product"'
     ],
     "animal welfare": [
-        "animal testing", "cruelty", "fur", "zoo", "factory farming", "endangered", "animal abuse"
-    ],
-    "all": [] 
+        '"animal testing"', "cruelty", "fur", "zoo", '"factory farming"', "endangered", '"animal abuse"'
+    ]
 }
