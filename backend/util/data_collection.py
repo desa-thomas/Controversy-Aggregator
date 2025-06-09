0dd481accf37a6d488df0396e4427caf7d156f74
desa-thomas <desa2thomas@gmail.com>
2025-06-09 18:10:33 -0400

    Added request timeouts, and error handling for `get_description` and `get_fortune_500` functions

--- backend/util/data_collection.py
+++ backend/util/data_collection.py
@@ -3,6 +3,7 @@ Thomas De Sa 2025-06-05
 
 Company data collection functions"""
 
+import time
 from bs4 import BeautifulSoup
 import requests
 import re
@@ -13,56 +14,81 @@ import io
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 
-def get_data(company: str):
-    """Scrape company data from wikipedia (description, logo, industries)
+def get_description(company: str):
+    """Scrape company description from wikipedia (description, logo)
 
     Args:
         company (str): Name of company
 
     Returns:
-        _type_: _description_
+        description (str): descriptioon of company (from wikipedia)
     """
 
     description = None
-    url = f"https://en.wikipedia.org/wiki/{company}"
-    res = requests.get(url)
+    try:
+        url = f"https://en.wikipedia.org/wiki/{company}_(company)"
+        res = requests.get(url, timeout=10)
+        print(f"{company} code: {res.status_code}")
 
-    print(f"code: {res.status_code}")
+        #If {company}_(company) not found, just search for company
+        if res.status_code == 404:
+            time.sleep(.5)
+            url = f"https://en.wikipedia.org/wiki/{company}"
+            res = requests.get(url, timeout=10)
+            print(f"{company} code: {res.status_code}")
 
-    if res.status_code == 200:
-        soup = BeautifulSoup(res.content, "html.parser")
 
-        content_div = soup.find(
-            "div", {"class": "mw-content-ltr mw-parser-output"})
-        description = content_div.find("p", {"class": ""})
+        if res.status_code == 200:
+            soup = BeautifulSoup(res.content, "html.parser")
 
-        # Paragraph found
-        if description is not None:
-            description = description.get_text()
+            content_div = soup.find(
+                "div", {"class": "mw-content-ltr mw-parser-output"})
+            description = content_div.find("p", {"class": ""})
 
-            # remove citations brackets ([9]) and pronounciation
-            description = re.sub(r'\[\d+\]', '', description)
-            description = re.sub(r'\(([^()]*\/[^()]*?)\)', '', description)
-            description = re.sub(r'\s{2,}', ' ', description).strip()
+            # Paragraph found
+            if description is not None:
+                description = description.get_text()
 
+                # remove citations brackets ([9]) and pronounciation
+                description = re.sub(r'\[\d+\]', '', description)
+                description = re.sub(r'\(([^()]*\/[^()]*?)\)', '', description)
+                description = re.sub(r'\s{2,}', ' ', description).strip()
+
+    except requests.exceptions.RequestException as e: 
+        print(f"Failed to fetch company {company}")
+        
+    ##TODO
+    #get company logo from wiki
     return description
 
 
 def get_fortune_500():
-    #IN PROGRESSS
-    #FIND OUT WHY THAT LINK DOES NOT GIVE THE TABLE
-    url = "https://www.50pros.com/fortune500"
-    res = requests.get(url)
+    """Return 2d array of fotune 500 companies from 
+    "https://sheet2site.com/api/v3/index.php?key=1S-vhiXvvKFDczI6vAK_dZlGqe3ftxNraArOZIVGivGw&g=1&e=1&e=1"
+    
+    Returns:
+        fortune_500 (list[list[str]]): 2d array of fortune 500 companies
+    """
+    fortune_500 = []
+    
+    #Iframe from https://www.50pros.com/fortune500
+    url = "https://sheet2site.com/api/v3/index.php?key=1S-vhiXvvKFDczI6vAK_dZlGqe3ftxNraArOZIVGivGw&g=1&e=1&e=1"
+    res = requests.get(url, timeout=10)
     print(f"code: {res.status_code}")
 
     if res.status_code == 200:
         soup = BeautifulSoup(res.content, "html.parser")
+        table = soup.find("tbody")
         
-        print(soup.prettify())
         
-        
+        for row in table.find_all("tr"):
+            row_arr = []
+            for td in row.find_all("td"):
+                row_arr.append(td.text)
+            
+            fortune_500.append(row_arr)
+
+    return fortune_500
 if __name__ == "__main__":
-    # description = get_data("google")d
-    # print(description)
-    
-    get_fortune_500()
+    description = get_description("Citi")
+    print(description)
