a862338c64a0085ca9f565f662872b4dedfff4ee
desa-thomas <desa2thomas@gmail.com>
2025-06-13 19:22:23 -0400

    Updated `get_articles` to return array of article objects

--- backend/article.py
+++ backend/article.py
@@ -1,8 +1,11 @@
 """
 Thomas De Sa - 2025-06-04
+
+GNEW DOCS: gnews.io/docs/v4 
 """
 
 import requests
+import time 
 from ethics_categories import ETHICS_CATEGORIES
 from config import API_KEY
 
@@ -10,7 +13,7 @@ class Article:
 
     GNEWS_ENDPOINT = "https://gnews.io/api/v4/search"
     
-    def __init__(self, company: str, headline: str, url: str, source: str, categories: list[str], date_published:str):
+    def __init__(self, company: str, headline: str, url: str, source: str, categories: list[str], date_published:str, description:str, retrieved:float):
         """Initalize Article object
 
         Args:
@@ -23,10 +26,12 @@ class Article:
         """ 
         self.company = company
         self.headline = headline
+        self.description = description
         self.url = url
         self.source = source
         self.categories = categories
         self.date_published = date_published
+        self.retrieved = retrieved #time article was retrived from GNEWS
         
     def get_articles(company:str, category:str, session:requests.Session = None):
         """Static function. Get articles form GNEWS API relating to a specific company and ethical category
@@ -40,7 +45,10 @@ class Article:
             json ()
         """
         category = category.lower()
-        json = None
+        
+        articles = []
+        
+        #generated session
         gen = False
         
         if category not in ETHICS_CATEGORIES.keys() and category != "all":
@@ -60,10 +68,48 @@ class Article:
             
             if res.json:
                 json = res.json()
+                articles_json = json["articles"]
+                
+                for article in articles_json:
+                    categories = Article.get_categories(article['title'], article['description'])
+                    
+                    article_obj = Article(company, 
+                                          article["title"], 
+                                          article["url"], 
+                                          article["source"]["name"], 
+                                          categories,
+                                          article["publishedAt"],
+                                          article['description'],
+                                          time.time())
+                    articles.append(article_obj)
             
             if gen:
                 session.close()
+    
+        return articles
+    
+    def get_categories(headline:str, description:str):
+        """Get all categories for an article
+
+        Args:
+            headline (str): headline
+        """
+        categories = []
+        for category in ETHICS_CATEGORIES.keys():
+            
+            #if any keyword appears in description or headline
+            if any(kw for kw in ETHICS_CATEGORIES[category] if (kw.lower() in description.lower() or kw.lower() in headline.lower())) or category in description:
+                categories.append(category)
         
-        #TODO
-        #Convert list of json Article objects to python Article objects, return list of Article objects
-        return json, res.status_code
+        return categories
+    
+    def __str__(self):
+        s = f"""{"-"*15}
+    {self.company} - {self.categories}
+    {self.source} - {self.date_published}
+    {self.url}
+    **{self.headline}**
+    {self.description}
+        """
+        
+        return s 
